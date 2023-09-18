9f60fc5e23f21b01eb34bb436dd8e933ec8acada
anastasiiaxfr <anastasiiaberest@gmail.com>
2023-09-18 13:07:39 +0300

    news sp

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,48 @@ if (collapse_toggle) {
     });
 };
 
+//MENU
+let menu_toggle = document.querySelector('.menu__toggle');
+let menu = document.querySelector('.menu');
+menu_toggle.addEventListener('click', function(){
+    menu.classList.toggle('active');
+    this.classList.toggle('active');
+});
+document.addEventListener("click", function (e) {
+    if (!menu.contains(e.target) && !menu_toggle.contains(e.target)) {
+      menu.classList.remove("active");
+      menu_toggle.classList.remove("active");
+    }
+});
 
+//FANCYBOX
+document.querySelectorAll(".trigger").forEach(i => i.addEventListener("click", () => {
+    let video = i.dataset.video;
+    Fancybox.show([
+      {
+        src: video,
+        thumb: "",
+        type: "video",
+        ratio: 16 / 10,
+      }
+    ]);
+}));  
+
+//SWIPER
+var splide = new Splide( '.splide', {
+    direction: 'ttb',
+    height: '650px',
+    focus: 'center',
+    wheel: true,
+    arrows: false,
+    padding: '1rem',
+    breakpoints: {
+		480: {
+			autoHeight: true,
+            padding: '1rem',
+            height: '320px',
+		},
+    }
+});
+  
+splide.mount();
---
+++ tina/config.js
@@ -0,0 +1,280 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.HUGO_PARAMS_TINA_CLIEN_ID, 
+  token: process.env.HUGO_PARAMS_TINA_TOKEN, 
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "news",
+        label: "News",
+        path: "content/en/news",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: 'image',
+            label: 'Image',
+            name: 'img',
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+
+      {
+        name: "terms",
+        label: "Terms",
+        path: "content/en/terms",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: 'image',
+            label: 'Image',
+            name: 'img',
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+
+      {
+        name: "policy",
+        label: "Policy",
+        path: "content/en/policy",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: 'image',
+            label: 'Image',
+            name: 'img',
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+
+      {
+        name: "terms_of_use",
+        label: "Terms Of Use",
+        path: "content/en/terms-of-use",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: 'image',
+            label: 'Image',
+            name: 'img',
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+
+      {
+        name: "aml_policy",
+        label: "Aml Policy",
+        path: "content/en/aml-policy",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: 'image',
+            label: 'Image',
+            name: 'img',
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+
+      {
+        name: "cookie_policy",
+        label: "Cookie Policy",
+        path: "content/en/cookie-policy",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: 'image',
+            label: 'Image',
+            name: 'img',
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+
+      {
+        name: "risk_warnings",
+        label: "Risk Warnings",
+        path: "content/en/risk-warnings",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: 'image',
+            label: 'Image',
+            name: 'img',
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+
+      {
+        name: "about_us",
+        label: "About Us",
+        path: "content/en/about-us",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            required: true,
+          },
+          {
+            type: 'image',
+            label: 'Image',
+            name: 'img',
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
